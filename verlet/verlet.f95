c44191a392c56dee93d5f1d0b953fc2934dbf0f5
abbas baraki <abbas.daryani7799@gmail.com>
2025-11-07 16:56:02 +0330

    modified program verlet

--- verlet/verlet.f95
+++ verlet/verlet.f95
@@ -1,7 +1,43 @@
 PROGRAM verlet
   IMPLICIT NONE ! inline comment
-  ! declare your variable here
+  INTEGER, PARAMETER :: wp =SELECTED_REAL_KIND (p=13, r=300)
+  REAL (KIND=wp) :: tau
+  REAL (KIND=wp) :: m
+  REAL (KIND=wp) :: x, y, z, xn, yn, zn, v_x, v_y, v_z, v_xn, v_yn, v_zn, f_x, f_y, f_z, f_xn, f_yn, f_zn
+  INTEGER :: k, n
 
-  ! write your instructions here
+  tau = 0.2_wp
+  m = 1
+  x = 0
+  y = 0
+  z = 0
+  v_x = 0
+  v_y = 0
+  v_z = 0
+  f_x = 0
+  f_y = 0.1
+  f_z = 0
+  f_xn = 0
+  f_yn = 0.1
+  f_zn = 0
+  n = 100
 
-END PROGRAM verlet
+  DO k = 1, n
+    xn = x + tau * v_x + tau ** 2 * f_x / (2 * m)
+    yn = y + tau * v_y + tau ** 2 * f_y / (2 * m)
+    zn = z + tau * v_z + tau ** 2 * f_z / (2 * m)
+    v_xn = v_x + tau / (2 * m) * (f_x + f_xn)
+    v_yn = v_y + tau / (2 * m) * (f_y + f_yn)
+    v_zn = v_z + tau / (2 * m) * (f_z + f_zn)
+
+    PRINT *, "The x, y and z positions of the particle at the", k, "-th iteration at time =", tau * k, "are:"
+    PRINT *, xn
+    PRINT *, yn
+    PRINT *, zn
+    PRINT *, v_xn
+    PRINT *, v_yn
+    PRINT *, v_zn
+
+  END DO
+
+END PROGRAM VERLET
